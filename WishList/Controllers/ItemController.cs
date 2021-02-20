bace640436d1f70cbf2f4ab113f75fe06e6ab72b
Tamilalagan S <M1046852@mindtree.com>
2021-02-21 00:39:38 +0530

    4

--- WishList/Controllers/ItemController.cs
+++ WishList/Controllers/ItemController.cs
@@ -1,16 +1,22 @@
 ﻿using System.Linq;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using WishList.Data;
+using WishList.Models;
 
 namespace WishList.Controllers
 {
+    [Authorize]
     public class ItemController : Controller
     {
         private readonly ApplicationDbContext _context;
+        private readonly UserManager<ApplicationUser> _userManager;
 
-        public ItemController(ApplicationDbContext context)
+        public ItemController(ApplicationDbContext context, UserManager<ApplicationUser> userManager)
         {
             _context = context;
+            _userManager = userManager;
         }
 
         public IActionResult Index()
